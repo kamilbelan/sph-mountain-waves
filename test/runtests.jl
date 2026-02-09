8949683e0f7d8015ecccb0ac319e7bda451d9cb3
Kamil Belán <kamil_belan@fastmail.org>
2026-02-09 20:24:08 +0100

    cleared tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,20 +3,20 @@ using DrWatson, Test
 
 # Here you include files using `srcdir`
 #include(srcdir("full_hopkins_perturbed.jl"))
-include(scriptsdir("full_hopkins_perturbed_witch.jl"))
-include(scriptsdir("hopkins_perturbed_witch.jl"))
-include(scriptsdir("hopkins_total_witch.jl"))
-include(scriptsdir("pavelka_total_witch.jl"))
-include(scriptsdir("wcsph_perturbed_witch.jl"))
+#include(scriptsdir("full_hopkins_perturbed_witch.jl"))
+#include(scriptsdir("hopkins_perturbed_witch.jl"))
+#include(scriptsdir("hopkins_total_witch.jl"))
+#include(scriptsdir("pavelka_total_witch.jl"))
+#include(scriptsdir("wcsph_perturbed_witch.jl"))
 
 # Run test suite
-println("Starting tests")
-ti = time()
+#println("Starting tests")
+#ti = time()
 
-@testset "SPH tests" begin
-    @test 1 == 1
-end
+#@testset "SPH tests" begin
+#    @test 1 == 1
+#end
 
-ti = time() - ti
-println("\nTest took total time of:")
-println(round(ti/60, digits = 3), " minutes")
+#ti = time() - ti
+#println("\nTest took total time of:")
+#println(round(ti/60, digits = 3), " minutes")
